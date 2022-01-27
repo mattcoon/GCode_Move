53f8c5fb603cd63ee8d1a31907f58962fcee2bb2
mattcoon <66556546+mattcoon@users.noreply.github.com>
2022-01-26 19:13:07 -0500

    add clean option to put object based at 0,0

--- gcode_move.py
+++ gcode_move.py
@@ -4,6 +4,10 @@
 # - scaling feed, extrusion, or X,Y,Z
 # - analysis only mode with statistics before and after modification
 # - runs command line or interactive
+# TODO: scale based on end size width or height
+# TODO: sanity check for -c clean to not conflict with offsets and throw error
+# TODO: enable individual interactive for missing components of commandline
+
 from pathlib import Path
 import re
 import sys
@@ -21,8 +25,12 @@ def findInt (str,param):
 
 
 def ProcessFile (filenameIn, filenameOut):
+    
     bLaserOn=False
     bScaledXYZ=False
+    deltaX = offsetX
+    deltaY = offsetY
+    deltaZ = offsetZ
 
     finMinX  = LimMaxX
     initMinX = finMinX
@@ -41,7 +49,7 @@ def ProcessFile (filenameIn, filenameOut):
 
     if scaleXYZ != 1:
         bScaledXYZ = True
-    if onlyAnalyse == False:
+    if bAnalyseOnly == False:
         fileOut = open(Path(filenameOut),'w')
         print("Writing to "+filenameOut)
 
@@ -63,6 +71,15 @@ def ProcessFile (filenameIn, filenameOut):
                             case 'Z':
                                 initMinZ = min(initMinZ,position)
                                 initMaxZ = max(initMaxZ,position)
+        
+        # if no positions found in axis, min will be max, reset that
+        initMinX = min(initMinX,initMaxX)
+        initMinY = min(initMinY,initMaxY)
+        initMinZ = min(initMinZ,initMaxZ)
+        if bCleanMode:
+            deltaX = -initMinX
+            deltaY = -initMinY
+            deltaZ = -initMinZ
         fileIn.seek(0)
         # reset file if offset searched
 
@@ -77,7 +94,7 @@ def ProcessFile (filenameIn, filenameOut):
                             break
                         if parts[0:1] == 'G':
                             # handle laser by making g0 g1 and threshold to completely turn off laser
-                            if laserMode:
+                            if bLaserMode:
                                 if bLaserOn:
                                     lineNew = 'G1'
                                 else:
@@ -132,8 +149,11 @@ def ProcessFile (filenameIn, filenameOut):
                 #check for laser off
                 if linesplit[0] == "M107":
                     bLaserOn = False
-                if onlyAnalyse == False:
+                if bAnalyseOnly == False:
                     fileOut.write(line)
+        finMinX = min(finMinX,finMaxX)
+        finMinY = min(finMinY,finMaxY)
+        finMinZ = min(finMinZ,finMaxZ)
         print('Scaled by '+'{:.2f}'.format(scaleXYZ)+'.')
         print('Initial')
         print('Min X: {0:.2f} Max X: {1:.2f} width: {2:.2f}'.format(initMinX, initMaxX, initMaxX-initMinX))
@@ -153,9 +173,9 @@ def Scale (position,scale,limLow,LimHi):
     return round(max(limLow,min(LimHi,position*scale)),6)
     
 
-deltaX = 0.0
-deltaY = 0.0
-deltaZ = 0.0
+offsetX = 0.0
+offsetY = 0.0
+offsetZ = 0.0
 scaleXYZ = 1.0
 scaleF = 1.0
 scaleE = 1.0
@@ -167,8 +187,10 @@ LimMaxE = 50000.0
 minOn = 0.0
 filenameIn = ''
 filenameOut = ''
-onlyAnalyse = False
-laserMode = False
+bAnalyseOnly = False
+bCleanMode = False
+bLaserMode = False
+
 if len(sys.argv) > 1:
     for argument in sys.argv:
         keyword = argument[0:2]
@@ -179,38 +201,41 @@ if len(sys.argv) > 1:
             case '-o':
                 filenameOut = userData
             case '-X':
-                deltaX = float(userData)
+                offsetX = float(userData)
             case '-Y':
-                deltaY = float(userData)
+                offsetY = float(userData)
             case '-Z':
-                deltaZ = float(userData)
+                offsetZ = float(userData)
             case '-F':
                 deltaF = float(userData)
             case '-E':
                 deltaE = float(userData)
             case '-h':
                 print('gcode_move -iInputFile -oOutputFile -Xoffset -Yoffset -Zoffset -FFeedrate -EExtruderrate')
-                print('           -aAnalyseOnly -lLaserlowerLimit -sScaleXYZ')
+                print('           -aAnalyseOnly -lLaserlowerLimit -sScaleXYZ -cClean')
                 print('Feedrate are in percent. Offset in mm.')
                 print('laser affects fanspeed 0-255 before turning off laser and puts in laser mode. ')
                 print('G1 while laser is off will become G0. use -l0 to keep laser PWMs but use G1/G0 substitutions')
                 print('XYZ scaling will treat as a mulitpler for any present components while keeping offsets. Note for 3d')
                 print('the first line is in scope and will prevent use of offset for scaling')
+                print('Clean mode removes all offsets and sets object to lowest possible point.')
                 quit()
             case '-a':
-                onlyAnalyse = True
+                bAnalyseOnly = True
+            case '-c':
+                bCleanMode = True
             case '-l':
                 minOn = float(userData)
-                laserMode = True
+                bLaserMode = True
             case '-s':
                 scaleXYZ = float(userData)
 else:
     print('no args - defaults in ( )')
-    userIn = input('X Offset ({:}):'.format(str(deltaX)))
+    userIn = input('X Offset ({:}):'.format(str(offsetX)))
     if userIn != '': deltaX = float(userIn) 
-    userIn = input('Y Offset ({:}):'.format(str(deltaY)))
+    userIn = input('Y Offset ({:}):'.format(str(offsetY)))
     if userIn != '': deltaY = float(userIn) 
-    userIn = input('Z Offset ({:}):'.format(str(deltaZ)))
+    userIn = input('Z Offset ({:}):'.format(str(offsetZ)))
     if userIn != '': deltaZ = float(userIn) 
     userIn = input('XYZ Scaling ({:}):'.format(str(scaleXYZ)))
     if userIn != '': scaleXYZ = float(userIn) 
